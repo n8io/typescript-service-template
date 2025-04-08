107ecd57042bfbc0da693a6c37f038c4027243ad
Nate Clark <n8io@users.noreply.github.com>
2025-04-08 18:44:33 -0400

    feat(NATE-032): ✨ Add config validation

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,10 +1,19 @@
-type Config = {
-  DATABASE_URL: string
+import { z } from 'zod'
+import { AppConfigIncompleteError } from '../models/custom-error.ts'
+
+const schemaConfig = z.object({
+  DATABASE_URL: z.string().url(),
+})
+
+type Config = z.infer<typeof schemaConfig>
+
+const results = schemaConfig.safeParse(process.env)
+
+if (!results.success) {
+  throw new AppConfigIncompleteError(results.error)
 }
 
-const config: Config = {
-  DATABASE_URL: process.env.DATABASE_URL ?? '',
-}
+const config = results.data
 
 export type { Config }
 export { config }
