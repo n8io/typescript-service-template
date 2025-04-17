a374f44a7d1143b801204c83fca3d79a5d9a396b
Nate Clark <n8io@users.noreply.github.com>
2025-04-16 20:39:44 -0400

    feat(NATE-049): ✨ Refactor GID and ID generators to include example functions and improve validation

--- src/utils/generators/gid.test.ts
+++ src/utils/generators/gid.test.ts
@@ -1,7 +1,17 @@
-import { isValid } from 'ulid'
-import { gid } from './gid.ts'
+import { GID_DELIMITER, GID_PREFIX, gid } from './gid.ts'
+
+const isValid = (gid: string) => {
+  const [prefix, uuid] = gid.trim().split(GID_DELIMITER)
+
+  if (!prefix || !uuid) {
+    return false
+  }
+
+  return prefix === GID_PREFIX && /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/.test(uuid)
+}
+
 describe('gid', () => {
-  it('should generate a valid ulid', () => {
+  it('should generate a valid GID', () => {
     const generatedGid = gid()
 
     expect(isValid(generatedGid)).toBe(true)
@@ -9,11 +19,12 @@ describe('gid', () => {
 
   it('should generate unique GIDs', () => {
     const generatedGids = new Set<string>()
+    const generations = 1_000
 
-    for (let i = 0; i < 100; i++) {
+    for (let i = 0; i < generations; i++) {
       generatedGids.add(gid())
     }
 
-    expect(generatedGids.size).toBe(100)
+    expect(generatedGids.size).toBe(generations)
   })
 })
