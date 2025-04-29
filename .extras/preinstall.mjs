7298d1f274b2b96e2f6d49090b9948aa87e5357a
Nate Clark <n8io@users.noreply.github.com>
2025-04-29 12:08:59 -0400

    feat(NATE-068): ✨ Update npm version constraint in package.json and package-lock.json to use caret notation

--- .extras/preinstall.mjs
+++ .extras/preinstall.mjs
@@ -1,6 +1,7 @@
 // This script updates the package.json file to set the node version in the engines field
 // This allows us to have a single source of truth for the node version (.nvmrc)
 
+import { execSync } from 'node:child_process'
 import { readFileSync, writeFileSync } from 'node:fs'
 import { dirname, resolve } from 'node:path'
 import { fileURLToPath } from 'node:url'
@@ -11,7 +12,10 @@ const nvmRcPath = resolve(__dirname, '../.nvmrc')
 const packageJsonPath = resolve(__dirname, '../package.json')
 const nodeVersion = readFileSync(nvmRcPath, 'utf8').trim().replace('v', '')
 const pkg = JSON.parse(readFileSync(packageJsonPath, 'utf8'))
+const npmVersion = execSync('npm --version', { encoding: 'utf8' }).trim()
+const majorNpmVersion = npmVersion.split('.')[0]
 
 pkg.engines.node = `^${nodeVersion}`
+pkg.engines.npm = `^${majorNpmVersion}`
 
 writeFileSync(packageJsonPath, `${JSON.stringify(pkg, null, 2)}\n`)
